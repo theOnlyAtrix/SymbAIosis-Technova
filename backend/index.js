a557daee9581e6a1f7c285726f37446a95209cdb
Roger / theOnlyAtrix <strikez246@gmail.com>
2025-10-17 03:36:49 +0400

    Backend

--- backend/index.js
+++ backend/index.js
@@ -88,7 +88,6 @@ ${companyEmails && companyEmails.length > 0 ? `Team Emails: ${companyEmails.join
     }
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
